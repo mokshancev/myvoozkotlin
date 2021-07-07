aa8d33aa1fe6c3c237c82e88f08610d8d4e0d40a
aleksei <mokshantsev.aleksei@yandex.ru>
2021-07-07 17:37:57 +0500

    update architecture

---
+++ app/src/main/java/com/example/myvoozkotlin/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.myvoozkotlin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myvoozkotlin.databinding.FragmentMainBinding
+
+class MainFragment: Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
